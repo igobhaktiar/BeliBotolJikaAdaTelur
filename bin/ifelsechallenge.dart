c164b197bf8be757c860a39929a9e41755f29e58
Firgo Bhaktiar Hamsah <71805833+igobhaktiar@users.noreply.github.com>
2023-08-03 20:40:28 +0700

    Jika ada telur beli 6 Botol

---
+++ bin/ifelsechallenge.dart
@@ -0,0 +1,17 @@
+import 'package:ifelsechallenge/ifelsechallenge.dart' as ifelsechallenge;
+import 'dart:io';
+
+void main() {
+
+  int oilThatShouldBuy;
+  var egg = true;
+
+  if (egg){
+    oilThatShouldBuy = 6;
+    print('Beli $oilThatShouldBuy botol minyak goreng ');
+  } else {
+    oilThatShouldBuy = 1;
+    print('Beli $oilThatShouldBuy botol minyak goreng ');
+  }
+
+}
---
+++ test/ifelsechallenge_test.dart
@@ -0,0 +1,8 @@
+import 'package:ifelsechallenge/ifelsechallenge.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
